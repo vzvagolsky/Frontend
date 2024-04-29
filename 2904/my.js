b10ce605c242bc70d43aba534370004c712f0924
vtarlovsky <vzvagolsky@gmai.com>
2024-04-29 22:49:42 +0200

    Homework 2904

---
+++ 2904/my.js
@@ -0,0 +1,28 @@
+const button = document.getElementById('get-content');
+const factElement = document.getElementById('this-content');
+
+button.addEventListener('click', () => {
+
+    factElement.textContent = '';
+
+
+    fetch('https://catfact.ninja/fact')
+        .then(response => {
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            return response.json();
+        })
+        .then(data => {
+
+            factElement.textContent = data.fact;
+            factElement.classList.add('text');
+        })
+        .catch(error => {
+
+            factElement.textContent = 'Error: ' + error.message;
+            factElement.classList.add('error');
+        });
+});
